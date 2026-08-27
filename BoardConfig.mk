e89ee4ad17f1aedaa99c0fc8bf257369add3dd97
Sabar <31942277+sabarop@users.noreply.github.com>
2026-08-27 11:30:07 +0700

    common: Use the common device compatibility matrix from QCOM

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -83,7 +83,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     vendor/aosp/config/device_framework_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
-DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 
 # Kernel
 BOARD_KERNEL_CMDLINE := \
